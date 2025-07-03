6e3146f1f477e87a9289b6fef12d0af5b6af1ece
Yohander Teran <yteran@intra.polybarq.com>
2025-07-03 11:08:48 -0400

    ultimos cambios

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import express from 'express'
 import colors from 'colors'
+import cors, {CorsOptions} from 'cors'
+import morgan from 'morgan'
 import swaggerUi from 'swagger-ui-express'
 import swaggerSpec, {swaggerUiOptions} from './config/swagger'
 import router from './router'
@@ -22,9 +24,33 @@ connectBD()
 //Instancia de express
 const server = express()
 
+//Permitir conexiones
+
+const allowedOrigins = [
+    process.env.FRONTEND_URL,
+    'http://localhost:4000/'
+];
+
+
+const corsOptions : CorsOptions={
+    origin: function(origin,callback){
+        console.log(origin)
+        if(!origin || allowedOrigins.includes(origin)){
+            callback(null,true)
+            console.log('Permitir...')
+        }else{
+            callback(new Error('Error de CORS'))
+            console.log('Error de CORS: Dominio no autorizado')
+        }
+    }
+}
+
+server.use(cors(corsOptions))
+
 //Leer datos de formularios
 server.use(express.json())
 
+server.use(morgan('dev'))
 server.use('/api/products',router)
 
 //Docs
